dd5630063995dbc15b268314f235d69d1b72c6bf
ccamel <camel.christophe@gmail.com>
2026-08-17 11:34:49 +0200

    build(project): add Nix reproducible dev env

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "ccamel GitHub profile development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.actionlint
+              pkgs.bash-language-server
+              pkgs.deadnix
+              pkgs.git
+              pkgs.markdownlint-cli2
+              pkgs.marksman
+              pkgs.nil
+              pkgs.nixfmt
+              pkgs.statix
+              pkgs.yaml-language-server
+              pkgs.yamllint
+            ];
+          };
+        }
+      );
+    };
+}
